db6df72989bd87e182dce2787c435eabcac7306c
Flávia Ferreira <vinhaferreira@gmail.com>
2023-06-04 15:18:38 -0300

    02 Eventos
    Aprendendo a adicionar eventos aos elementos no jquery

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
   console.log(document.querySelector("header button"));
   console.log($("header button"));
+
+  document
+    .querySelector("header button")
+    .addEventListener("click", function (e) {});
+
+  $("header button").click(function () {
+    alert("Expandir formulario");
+  });
+
+  $("form").on("submit", function (e) {
+    console.log("submit");
+    e.preventDefault();
+  });
 });
