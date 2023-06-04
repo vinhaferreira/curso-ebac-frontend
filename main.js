3417d5502e21f3e8d02539a3b86a4804139bb8e1
Flávia Ferreira <vinhaferreira@gmail.com>
2023-06-04 15:08:58 -0300

    Aula 01 -seletores

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-$(document).ready((function(){
-  alert("Olá JQuery")
-})
+$(document).ready(function () {
+  console.log(document.querySelector("header button"));
+  console.log($("header button"));
+});
