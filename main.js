f55adce3f614f2e39f1a8a3dc3416702ef74f01e
Flávia Ferreira <vinhaferreira@gmail.com>
2023-06-04 15:27:35 -0300

    Aula 3: Animações e efeitos
    Formulario aparece quando aperta em nova imagem e desaparece quando aperta em cancelar

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
-  console.log(document.querySelector("header button"));
-  console.log($("header button"));
-
-  document
-    .querySelector("header button")
-    .addEventListener("click", function (e) {});
-
   $("header button").click(function () {
-    alert("Expandir formulario");
+    $("form").slideDown();
+  });
+
+  $("#botao-cancelar").click(function () {
+    $("form").slideUp();
   });
 
   $("form").on("submit", function (e) {
